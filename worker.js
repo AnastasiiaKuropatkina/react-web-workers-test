4d992e672c8fb91b315f65afb085e66610c5dc36
AnastasiiaKuropatkina <an_datsyuk@i.ua>
2021-12-11 15:46:53 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,19 @@
-function fibonacci(n) {
-    if (n < 2) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+function fibonacci(num) {
+    if (num === 1)
+        return 0;
+    if (num === 2)
+        return 1;
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 2;
+    while (i < num) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        i += 1;
+    }
+    return num2;
 }
 
 module.exports = class Worker {
